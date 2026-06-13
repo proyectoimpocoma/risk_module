7d1c3d8e4612319ef9626d3ab79891c79410b921
luissoler <luissoler57@gmail.com>
2026-06-13 17:23:28 -0500

    style: redesign risk submission UI with new corporate branding, updated input styles, and step-specific iconography.

--- static/src/js/risk_submission_vehicle.js
+++ static/src/js/risk_submission_vehicle.js
@@ -1,33 +1,79 @@
-(function () {
-    function initSemiTrailerSwitch() {
-        var valueInput = document.getElementById("has_semi_trailer");
-        var toggle = document.getElementById("has_semi_trailer_toggle");
-        var plateInput = document.getElementById("semi_trailer_plate");
-        var plateField = document.querySelector("[data-semi-trailer-field='1']");
+/** @odoo-module **/
 
-        if (!valueInput || !toggle || !plateInput || !plateField) {
+import publicWidget from "@web/legacy/js/public/public_widget";
+
+publicWidget.registry.RiskSubmissionVehicle = publicWidget.Widget.extend({
+    selector: '.risk-step-page',
+    events: {
+        'change #has_semi_trailer_toggle': '_onSemiTrailerToggle',
+        'click .btn-toggle-password': '_onTogglePassword',
+    },
+
+    /**
+     * @override
+     */
+    start: function () {
+        this._syncSemiTrailerState();
+        return this._super.apply(this, arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {Event} ev
+     */
+    _onSemiTrailerToggle: function (ev) {
+        this._syncSemiTrailerState();
+    },
+
+    /**
+     * @private
+     * @param {Event} ev
+     */
+    _onTogglePassword: function (ev) {
+        ev.preventDefault();
+        var $input = this.$('#satellite_password');
+        var $icon = this.$(ev.currentTarget).find('i');
+        
+        if ($input.attr('type') === 'password') {
+            $input.attr('type', 'text');
+            $icon.removeClass('fa-eye').addClass('fa-eye-slash');
+            this.$(ev.currentTarget).attr('aria-label', 'Ocultar contraseña');
+        } else {
+            $input.attr('type', 'password');
+            $icon.removeClass('fa-eye-slash').addClass('fa-eye');
+            this.$(ev.currentTarget).attr('aria-label', 'Mostrar contraseña');
+        }
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     */
+    _syncSemiTrailerState: function () {
+        var $valueInput = this.$('#has_semi_trailer');
+        var $toggle = this.$('#has_semi_trailer_toggle');
+        var $plateInput = this.$('#semi_trailer_plate');
+        var $plateField = this.$("[data-semi-trailer-field='1']");
+
+        if (!$valueInput.length || !$toggle.length || !$plateInput.length || !$plateField.length) {
             return;
         }
 
-        function syncState() {
-            var enabled = toggle.checked;
-            valueInput.value = enabled ? "yes" : "no";
-            plateInput.disabled = !enabled;
-            plateInput.required = enabled;
-            plateField.classList.toggle("is-disabled", !enabled);
+        var enabled = $toggle.prop('checked');
+        $valueInput.val(enabled ? "yes" : "no");
+        $plateInput.prop('disabled', !enabled);
+        $plateInput.prop('required', enabled);
+        $plateField.toggleClass("is-disabled", !enabled);
 
-            if (!enabled) {
-                plateInput.value = "";
-            }
+        if (!enabled) {
+            $plateInput.val("");
         }
-
-        toggle.addEventListener("change", syncState);
-        syncState();
-    }
-
-    if (document.readyState === "loading") {
-        document.addEventListener("DOMContentLoaded", initSemiTrailerSwitch);
-    } else {
-        initSemiTrailerSwitch();
-    }
-}());
+    },
+});
